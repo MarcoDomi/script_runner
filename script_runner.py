e2d32cc2b2a1cc0c951b523a234bd9fa29eb79bf
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-14 12:09:29 -0500

    scan utility_scripts dir for files

--- script_runner.py
+++ script_runner.py
@@ -2,4 +2,11 @@
 
 import sys
 import os
+import pathlib
+
+script_run_path = pathlib.Path(__file__).parent
+script_run_path = script_run_path / 'utility_scripts'
+
+for _, _, filenames in os.walk(script_run_path):
+    print(filenames)
 
