f4e368b8d56ac46cf02c96bda8efe1240d77386e
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-23 23:56:42 -0500

    run files without including .py extension

--- script_runner.py
+++ script_runner.py
@@ -3,18 +3,22 @@
 import sys
 import subprocess
 import pathlib
-from glob import glob
 
+curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
 
 
 if len(sys.argv) < 2: #may remove in final version
     print('Usage: python3 script_runner.py <filename.py>')
     exit()
 
-curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+
 filename = sys.argv[1]
-filename_set = glob(f'{filename}.py', root_dir=curr_path) #retrieve all files that end in '.py'
-target_script = curr_path.joinpath(sys.argv[1])
+
+if '.py' not in filename:
+    filename = filename + '.py'
+
+target_script = curr_path.joinpath(filename)
+
 result = subprocess.run(['python3', target_script], capture_output=True, check=True)
 print(result.stdout.decode())
 print(result.stderr)
