1a61d3b746d09678b32b85365ae61d202103d8c5
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-23 23:15:56 -0500

    remove utility_scripts

--- script_runner.py
+++ script_runner.py
@@ -6,15 +6,15 @@ import pathlib
 from glob import glob
 
 
-script_run_path = pathlib.Path(__file__).parent / "utility_scripts"  # use .parent attr to remove script_runner.py from path
-filename_set = set(glob('*.py', root_dir=script_run_path)) #retrieve all files that start with 'sc_' and end in '.py'
 
-if len(sys.argv) < 2: # may remove in final version
+if len(sys.argv) < 2: #may remove in final version
     print('Usage: python3 script_runner.py <filename.py>')
     exit()
 
-if sys.argv[1] in filename_set:
-    target_script = script_run_path.joinpath(sys.argv[1])
-    result = subprocess.run(['python3', target_script], capture_output=True, check=True)
-    print(result.stdout.decode())
-    print(result.stderr)
+curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+filename = sys.argv[1]
+filename_set = glob(f'{filename}.py', root_dir=curr_path) #retrieve all files that end in '.py'
+target_script = curr_path.joinpath(sys.argv[1])
+result = subprocess.run(['python3', target_script], capture_output=True, check=True)
+print(result.stdout.decode())
+print(result.stderr)
