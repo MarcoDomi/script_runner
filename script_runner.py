b59f451bf52c0d91308e5a2300624a1e88e1afcf
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-24 15:17:04 -0500

    create get_scriptPath function

--- script_runner.py
+++ script_runner.py
@@ -4,21 +4,23 @@ import sys
 import subprocess
 import pathlib
 
-curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+
+def get_scriptPath():
+    curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+    filename = sys.argv[1]
+
+    if ".py" not in filename:
+        filename = filename + ".py"
+
+    return curr_path.joinpath(filename)
+
 
 
 if len(sys.argv) < 2: #may remove in final version
     print('Usage: python3 script_runner.py <filename.py>')
     exit()
 
+script_path = get_scriptPath()
 
-filename = sys.argv[1]
-
-if '.py' not in filename:
-    filename = filename + '.py'
-
-target_script = curr_path.joinpath(filename)
-
-result = subprocess.run(['python3', target_script], capture_output=True, check=True)
-print(result.stdout.decode())
-print(result.stderr)
+result = subprocess.run(['python3', script_path], capture_output=True, check=True)
+print(result.stdout.decode().strip())
