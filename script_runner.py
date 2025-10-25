2a0b505d24aa97550b12c2e899aa3a3d4038e97c
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-25 14:51:43 -0500

    add tool_manager class

--- script_runner.py
+++ script_runner.py
@@ -4,23 +4,41 @@ import sys
 import subprocess
 import pathlib
 
+class tool_manager:
+    def __init__(self):
+        self.file_dict = {}
 
-def get_scriptPath():
-    curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+    def update_dict(self):
+        pass
+
+    def list_files(self):
+        file_list = []
+
+        for k in self.file_dict:
+            pass
+
+def get_filename():
     filename = sys.argv[1]
 
     if ".py" not in filename:
         filename = filename + ".py"
 
-    return curr_path.joinpath(filename)
+    return filename
 
+
+def get_scriptPath(filename):
+    curr_path = pathlib.Path(__file__).parent #use .parent attr to remove script_runner.py from path
+
+    return curr_path.joinpath(filename)
 
 
 if len(sys.argv) < 2: #may remove in final version
     print('Usage: python3 script_runner.py <filename.py>')
     exit()
 
-script_path = get_scriptPath()
+
+filename = get_filename()
+script_path = get_scriptPath(filename)
 
 result = subprocess.run(['python3', script_path], capture_output=True, check=True)
 print(result.stdout.decode().strip())
