bc6ce3b6d7f5f2eeac9f697370581952579a7fcd
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2025-10-28 17:07:21 -0500

    add _create_dir_set and _create_dict_set

--- script_runner.py
+++ script_runner.py
@@ -4,16 +4,43 @@ import sys
 import subprocess
 import pathlib
 
+CURR_PATH = pathlib.Path(__file__).parent
 class tool_manager:
     def __init__(self):
         self.file_dict = {}
 
     def update_dict(self):
-        pass
+        dict_set = self._create_dict_set()
+        dir_set = self._create_dir_set()
+
+        diff_set = dir_set.difference(dict_set) #filenames that are present in directory but not in file dictionary
+
+        for f in diff_set:
+            pass
 
     def list_files(self):
-        for k in self.file_dict:
-            print(f'{self.file_dict[k]} - {k}')
+        '''print a list of files and it's abbreviation'''
+        for filename, abbrev in self.file_dict.items():
+            print(f"{filename} - {abbrev}")
+
+    def _create_dict_set(self): 
+        '''returns a set created from file dictionary'''
+
+        dict_set = set()
+        for _, value in self.file_dict:
+            dict_set.add(value)
+
+        return dict_set
+
+    def _create_dir_set(self):
+        '''iterate thru all elements in dictionary to create a set'''
+        dir_set = set()
+        for f in pathlib.Path.iterdir(CURR_PATH):
+            if f.name[-3:] == '.py' and f.name != 'script_runner.py':
+                dir_set.add(f.name)
+
+        return dir_set
+
 
 def get_filename():
     filename = sys.argv[1]
@@ -30,13 +57,21 @@ def get_scriptPath(filename):
     return curr_path.joinpath(filename)
 
 
-if len(sys.argv) < 2: #may remove in final version
-    print('Usage: python3 script_runner.py <filename.py>')
-    exit()
+def main():
+    if len(sys.argv) < 2: #may remove in final version
+        print('Usage: python3 script_runner.py <filename.py>')
+        exit()
+
+    filename = get_filename()
+    script_path = get_scriptPath(filename)
+
+    result = subprocess.run(['python3', script_path], capture_output=True, check=True)
+    print(result.stdout.decode().strip())
+
+def class_tester(): #NOTE delete later
+    foo = tool_manager()
+    foo.update_dict()
 
 
-filename = get_filename()
-script_path = get_scriptPath(filename)
-
-result = subprocess.run(['python3', script_path], capture_output=True, check=True)
-print(result.stdout.decode().strip())
+# main()
+class_tester()
